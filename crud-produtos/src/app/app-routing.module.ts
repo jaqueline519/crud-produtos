f2cd586b981f1fa7e8baca10d55a07312c895bb0
jaqueline519 <jaquelinesantos2810@gmail.com>
2022-12-08 14:21:53 -0300

    arquivos que faltaram no ultimo commit

--- crud-produtos/src/app/app-routing.module.ts
+++ crud-produtos/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabelaProdutosComponent } from '../shared/containers/tabela-produtos/tabela-produtos.component';
+import { CadastrarProdutoComponent } from '../shared/containers/cadastrar-produto/cadastrar-produto.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
       {
         path: 'lista-produtos',
         component: TabelaProdutosComponent
+      },
+      {
+        path: 'cadastrar-produtos',
+        component: CadastrarProdutoComponent
       },
       {
         path: '**',
